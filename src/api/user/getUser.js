582504852cf365c1bcd67e6e779f887218951953
Manuel <luisparedescuenca@gmail.com>
2025-07-03 12:27:32 -0300

    Timeout for fetch

--- src/api/user/getUser.js
+++ src/api/user/getUser.js
@@ -1,14 +1,21 @@
 export const getUser = async (token) => {
 	const url = 'https://e-mercado.onrender.com/api/user/me';
 
+	//timeout for fetch
+	const timeoutPromise = new Promise((_, reject) =>
+		setTimeout(() => reject(new Error('Timeout finished')), 10000)
+	);
+
+	//fetch for user
 	const params = {
 		method: 'GET',
 		headers: {
 			Authorization: token,
 		}
 	};
+	const fetchUser = fetch(url, params);
 
-	const response = await fetch(url, params);
+	const response = await Promise.race([fetchUser, timeoutPromise]);
 	const result = await response.json();
 
 	if (response.status !== 200) throw result;
